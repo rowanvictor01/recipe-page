e43a5aa81319232cbda5d40ad6ff9095915e7413
Rowan Victor <209355544+rowanvictor01@users.noreply.github.com>
2025-08-13 00:13:42 +0800

    Design intro and prep time sections and add tailwind.config.js file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./*.html", "./assets/**/*.css"], // match all your HTML and CSS
+  theme: {
+    extend: {
+      fontFamily: {
+        youngSerif: ["young-serif", "sans-serif"],
+        outfit: ["outfit", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
